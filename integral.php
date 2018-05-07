c4cf78dd909685a71f499df5bedb3dbd9153b2af
Gabriel Johnson <leirbag75@gmail.com>
2018-05-07 13:47:38 -0500

    Use variables for certain magic constants

--- integral.php
+++ integral.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: text/plain');
-echo shell_exec(sprintf('/home/gabriel/Desktop/spiroffs-project/src/calculate-integral %s %s',
+$code_path = '/home/gabriel/Desktop/spiroffs-project/src/calculate-integral';
+echo shell_exec(sprintf("$code_path %s %s",
   implode(' ', array_map("escapeshellarg", $_GET['a'])),
   implode(' ', array_map("escapeshellarg", $_GET['b']))
 ));
