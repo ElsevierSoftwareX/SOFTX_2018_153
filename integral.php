627de29012ee3bae9de5bbcfea2477d745265728
Gabriel Johnson <leirbag75@gmail.com>
2018-08-07 16:27:24 -0700

    Remove extra Macaulay 2 code and web interface

--- integral.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-$code_path = '/home/gabriel/Desktop/spiroffs-project/src/calculate-integral';
-echo shell_exec(sprintf("$code_path %s %s",
-  implode(' ', array_map("escapeshellarg", $_GET['a'])),
-  implode(' ', array_map("escapeshellarg", $_GET['b']))
-));
-?>
